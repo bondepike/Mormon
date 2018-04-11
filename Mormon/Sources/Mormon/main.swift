0cc8815737b354cf7212802a29d583b052ed8839
Adrian Fjelle Evensen <guleadrian@gmail.com>
2018-04-11 16:59:25 +0200

    Initial Commit

---
+++ Mormon/Sources/Mormon/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mormon
+//
+//  Created by Adrian Evensen on 11/04/2018.
+//  Copyright © 2018 Adrianf. All rights reserved.
+//
+
+import Foundation
+import NIO
+import NIOHTTP1
+
